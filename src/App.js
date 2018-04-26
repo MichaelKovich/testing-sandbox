e8564baea0e40592501f7db125f73df6c4f7ad94
Michael Kovich <kovich@michaelkovich.com>
2018-04-26 11:12:59 -0500

    Implemented several changes via a TDD process.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: false,
       todos: [
         // {
         //   id: 1,
@@ -17,10 +19,19 @@ class App extends Component {
       ],
     };
   }
+
+  componentDidMount() {
+    axios
+      .get('/api/todos')
+      .then(res => this.setState({todos: res.data}))
+      .catch(err => this.setState({error: true}));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header" />
+        {this.state.error && <span className="error">Error!</span>}
         <input className="new_todo" placeholder="Add new Todo" />
         <List todos={this.state.todos} />
       </div>
